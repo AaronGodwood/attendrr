7112b21420768319adc6923de8a2a3dadf94d27c
Aaron Godwood <godwoodaaron@gmail.com>
2025-11-27 12:55:23 +0000

    set up UI

--- lib/features/timetable/presentation/providers/timetable_provider.dart
+++ lib/features/timetable/presentation/providers/timetable_provider.dart
@@ -1,4 +1,17 @@
-// Timetable provider
-class TimetableProvider {
-  // TODO: Implement timetable provider
+import 'package:flutter/material.dart';
+
+class TimetableProvider extends ChangeNotifier {
+  List<dynamic> _lectures = [];
+
+  List<dynamic> get lectures => _lectures;
+
+  Future<void> loadTimetable() async {
+    // TODO: Implement timetable loading
+    notifyListeners();
+  }
+
+  Future<void> syncTimetable() async {
+    // TODO: Implement timetable sync
+    notifyListeners();
+  }
 }
