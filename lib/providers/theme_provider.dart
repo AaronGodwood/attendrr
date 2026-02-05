9c5e4700c6d7aae745e2778826c5cb302e281ac5
Aaron Godwood <godwoodaaron@gmail.com>
2026-02-05 23:21:30 +0000

    added high contrast theme

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -2,18 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../utils/constants.dart';
 
+enum AppThemeMode {
+  light,
+  dark,
+  highContrast,
+  system,
+}
+
 class ThemeProvider extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.system;
+  AppThemeMode _appThemeMode = AppThemeMode.system;
+  bool _useHighContrast = false;
 
-  ThemeMode get themeMode => _themeMode;
+  ThemeMode get themeMode => _useHighContrast ? ThemeMode.light : _themeMode;
+  AppThemeMode get appThemeMode => _appThemeMode;
+  bool get useHighContrast => _useHighContrast;
 
   String get themeModeLabel {
-    switch (_themeMode) {
-      case ThemeMode.light:
+    switch (_appThemeMode) {
+      case AppThemeMode.light:
         return 'Light';
-      case ThemeMode.dark:
+      case AppThemeMode.dark:
         return 'Dark';
-      case ThemeMode.system:
+      case AppThemeMode.highContrast:
+        return 'High Contrast';
+      case AppThemeMode.system:
         return 'System default';
     }
   }
@@ -30,13 +43,24 @@ class ThemeProvider extends ChangeNotifier {
       if (savedMode != null) {
         switch (savedMode) {
           case 'light':
+            _appThemeMode = AppThemeMode.light;
             _themeMode = ThemeMode.light;
+            _useHighContrast = false;
             break;
           case 'dark':
+            _appThemeMode = AppThemeMode.dark;
             _themeMode = ThemeMode.dark;
+            _useHighContrast = false;
+            break;
+          case 'highContrast':
+            _appThemeMode = AppThemeMode.highContrast;
+            _themeMode = ThemeMode.light;
+            _useHighContrast = true;
             break;
           case 'system':
+            _appThemeMode = AppThemeMode.system;
             _themeMode = ThemeMode.system;
+            _useHighContrast = false;
             break;
         }
         notifyListeners();
@@ -47,10 +71,30 @@ class ThemeProvider extends ChangeNotifier {
     }
   }
 
-  Future<void> setThemeMode(ThemeMode mode) async {
-    if (_themeMode == mode) return;
+  Future<void> setThemeMode(AppThemeMode mode) async {
+    if (_appThemeMode == mode) return;
 
-    _themeMode = mode;
+    _appThemeMode = mode;
+
+    switch (mode) {
+      case AppThemeMode.light:
+        _themeMode = ThemeMode.light;
+        _useHighContrast = false;
+        break;
+      case AppThemeMode.dark:
+        _themeMode = ThemeMode.dark;
+        _useHighContrast = false;
+        break;
+      case AppThemeMode.highContrast:
+        _themeMode = ThemeMode.light;
+        _useHighContrast = true;
+        break;
+      case AppThemeMode.system:
+        _themeMode = ThemeMode.system;
+        _useHighContrast = false;
+        break;
+    }
+
     notifyListeners();
 
     try {
@@ -58,13 +102,16 @@ class ThemeProvider extends ChangeNotifier {
       String modeString;
 
       switch (mode) {
-        case ThemeMode.light:
+        case AppThemeMode.light:
           modeString = 'light';
           break;
-        case ThemeMode.dark:
+        case AppThemeMode.dark:
           modeString = 'dark';
           break;
-        case ThemeMode.system:
+        case AppThemeMode.highContrast:
+          modeString = 'highContrast';
+          break;
+        case AppThemeMode.system:
           modeString = 'system';
           break;
       }
