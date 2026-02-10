aa59835308aa5dcd2cce904af478975f97768570
Aaron Godwood <godwoodaaron@gmail.com>
2026-02-10 12:48:35 +0000

    added profile picture support

--- lib/providers/profile_provider.dart
+++ lib/providers/profile_provider.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' hide User;
 import '../repositories/user_repository.dart';
 import '../repositories/attendance_repository.dart';
 import '../models/models.dart';
@@ -13,6 +15,7 @@ class ProfileProvider extends ChangeNotifier {
   AttendanceStats? _stats;
   List<DailyAttendance>? _history;
   bool _isLoading = false;
+  bool _isUploading = false;
   String? _error;
 
   User? get user => _user;
@@ -21,6 +24,7 @@ class ProfileProvider extends ChangeNotifier {
   AttendanceStats? get stats => _stats;
   List<DailyAttendance>? get history => _history;
   bool get isLoading => _isLoading;
+  bool get isUploading => _isUploading;
   String? get error => _error;
 
   Future<void> loadProfile() async {
@@ -50,6 +54,52 @@ class ProfileProvider extends ChangeNotifier {
       await loadProfile();
     } catch (e) {
       _error = e.toString();
+      notifyListeners();
+    }
+  }
+
+  Future<void> updateAvatar(XFile imageFile) async {
+    _isUploading = true;
+    notifyListeners();
+
+    try {
+      final client = Supabase.instance.client;
+      final userId = client.auth.currentUser!.id;
+      final bytes = await imageFile.readAsBytes();
+      final ext = imageFile.path.split('.').last;
+      final path = '$userId/${DateTime.now().millisecondsSinceEpoch}.$ext';
+
+      await client.storage.from('avatars').uploadBinary(
+            path,
+            bytes,
+            fileOptions: const FileOptions(upsert: true),
+          );
+
+      final publicUrl = client.storage.from('avatars').getPublicUrl(path);
+      await _userRepo.updateProfile(avatarUrl: publicUrl);
+      await loadProfile();
+    } catch (e) {
+      _error = e.toString();
+      notifyListeners();
+    } finally {
+      _isUploading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> removeAvatar() async {
+    _isUploading = true;
+    notifyListeners();
+
+    try {
+      final client = Supabase.instance.client;
+      await client.from('profiles').update({'avatar_url': null}).eq('id', client.auth.currentUser!.id);
+      await loadProfile();
+    } catch (e) {
+      _error = e.toString();
+      notifyListeners();
+    } finally {
+      _isUploading = false;
       notifyListeners();
     }
   }
