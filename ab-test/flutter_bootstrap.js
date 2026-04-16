5a7de4195211c907de345c0efcd466c4b95eca8f
AaronGodwood <AaronGodwood@users.noreply.github.com>
2026-04-16 13:45:57 +0000

    deploy: 9f2e0bc570c671d4a61fc82dffd80b92b26edca2

--- ab-test/flutter_bootstrap.js
+++ ab-test/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2376086278" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1867406242" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
